67220897674158ed459782135c9fc7447d62ca77
Ashley Towner <git@ashtowner.com>
2023-06-11 12:20:20 +0930

    Revert "Added SSL files"
    This reverts commit 0ac90e0d334d4564b8a63894a9444c63773ff74d.

--- next.config.js
+++ next.config.js
@@ -1,24 +1,4 @@
-const fs = require('fs');
-
-let keyFile;
-let certFile;
-
-try {
-  keyFile = fs.readFileSync('/etc/letsencrypt/live/fullchain.pem');
-  certFile = fs.readFileSync('/etc/letsencrypt/live/privkey.pem');
-} catch {
-  console.error('Couldn\'t get SSL certificate files');
-}
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  secure: true,
-  server: {
-    https: {
-      key: keyFile,
-      cert: certFile
-    }
-  }
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
