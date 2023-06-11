0ac90e0d334d4564b8a63894a9444c63773ff74d
Ashley Towner <git@ashtowner.com>
2023-06-11 12:17:53 +0930

    Added SSL files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
+const fs = require('fs');
+
+let keyFile;
+let certFile;
+
+try {
+  keyFile = fs.readFileSync('/etc/letsencrypt/live/fullchain.pem');
+  certFile = fs.readFileSync('/etc/letsencrypt/live/privkey.pem');
+} catch {
+  console.error('Couldn\'t get SSL certificate files');
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  secure: true,
+  server: {
+    https: {
+      key: keyFile,
+      cert: certFile
+    }
+  }
+};
 
 module.exports = nextConfig;
