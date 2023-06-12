e0f176ea8ee7f7441126465a762c769e21c789cc
Ashley Towner <git@ashtowner.com>
2023-06-12 10:23:15 +0930

    Set up SSL / HTTPS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const next = require('next');
 
@@ -8,16 +9,40 @@ const app = next({ dev });
 
 const handle = app.getRequestHandler();
 
-const httpsOptions = {
-  key: fs.readFileSync(process.env.SSL_KEY),
-  cert: fs.readFileSync(process.env.SSL_CERT),
-}
+if (process.env.NODE_ENV === 'production') {
+  const sslKeyFile = process.env.SSL_KEY;
+  const sslCertFile = process.env.SSL_CERT;
 
-app.prepare().then(() => {
-  https.createServer(httpsOptions, (req, res) => {
-    handle(req, res);
-  }).listen(443, err => {
-    if (err) throw err;
-    console.log('> Ready on https://localhost:443');
+  if (!sslKeyFile || !sslCertFile) {
+    throw new Error('SSL_KEY and SSL_CERT must be set');
+  }
+
+  let httpsOptions;
+
+  httpsOptions = {
+    key: fs.readFileSync(process.env.SSL_KEY),
+    cert: fs.readFileSync(process.env.SSL_CERT),
+  };
+
+  app.prepare().then(() => {
+    https
+      .createServer(httpsOptions, (req, res) => {
+        handle(req, res);
+      })
+      .listen(443, (err) => {
+        if (err) throw err;
+        console.log('> Ready on https://localhost:443');
+      });
   });
-})
+} else {
+  app.prepare().then(() => {
+    http
+      .createServer((req, res) => {
+        handle(req, res);
+      })
+      .listen(3000, (err) => {
+        if (err) throw err;
+        console.log('> Ready on http://localhost:3000');
+      });
+  });
+}
