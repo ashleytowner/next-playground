c0e140c87e2b9e2c12ddf190955faf6787f581f1
Ashley Towner <git@ashtowner.com>
2023-06-12 10:39:42 +0930

    improved navlink styles

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,36 @@
 'use client';
+import classnames from '@/lib/classnames';
 import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+
+const NavLink = (props: { route: string; label: string }) => {
+	const pathname = usePathname();
+	const classname = classnames(
+		{
+			'text-secondary-400': pathname === props.route,
+			'border-b-2': pathname === props.route,
+			'border-secondary-400': pathname === props.route,
+		},
+		'pt-4',
+		'pb-4',
+		'pl-1',
+		'pr-1',
+		'no-underline',
+		'hover:text-secondary-400',
+	);
+	return (
+		<Link href={props.route} className={classname}>
+			{props.label}
+		</Link>
+	);
+};
 
 export default function NavBar() {
-  const currentRoute = usePathname();
-
-  return (
-    <nav className="bg-slate-50 dark:bg-slate-900 flex flex-row gap-3 p-4 shadow-slate-400 dark:shadow-slate-950 shadow">
-      <Link href="/">Home {currentRoute === '/' && '*'}</Link>
-      <Link href="/elo">Elo Calculator {currentRoute === '/elo' && '*'}</Link>
-      <Link href="/spotify">
-        Spotify Insights {currentRoute === '/spotify' && '*'}
-      </Link>
-    </nav>
-  );
+	return (
+		<nav className="bg-slate-50 pl-4 dark:bg-slate-900 flex flex-row gap-3 shadow-slate-400 dark:shadow-slate-950 shadow">
+			<NavLink route="/" label="Home" />
+			<NavLink route="/elo" label="Elo Calculator" />
+			<NavLink route="/spotify" label="Spotify Insights" />
+		</nav>
+	);
 }
